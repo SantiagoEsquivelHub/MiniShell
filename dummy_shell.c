8b5273a36e61ed95bfbf093d2eb21412dbaab9fd
Santiago Sánchez Esquivel <santiagosancheze8.1@gmail.com>
2022-05-26 15:32:39 -0500

    codigo del minishell

---
+++ dummy_shell.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <assert.h>
+#include "leercadena.h"
+#include <string.h>
+
+#define MAX 100
+
+int main(int argc, char *argv[])
+{
+    char **vector_cadenas;
+    char cadena[MAX];
+    char **listaBg;
+    char **procesosBg;
+    char *procesos[100];
+    int posicionVector = 1;
+    while (1)
+    {
+        pid_t pid, pidBG;
+        printf("> ");
+        leer_de_teclado(MAX, cadena);
+        vector_cadenas = de_cadena_a_vector(cadena);
+
+        if (strcmp("salir", vector_cadenas[0]) == 0)
+        {
+
+            printf("Gracias por usar mi dummy shell ;-)\n");
+            sleep(3);
+            char *const args[] = {"clear", NULL};
+            execvp(args[0], args);
+            break;
+        }
+
+        if (strcmp("tareas", vector_cadenas[0]) == 0)
+        {
+
+            printf("---------------------------------- \n");
+            printf("Procesos en segundo plano \n");
+
+            for (int i = 0; i < posicionVector; i++)
+            {
+                printf("%s, [%d] \n", procesos[posicionVector], posicionVector);
+            }
+
+            posicionVector++;
+
+            printf("---------------------------------- \n");
+        }
+
+        pid = fork();
+        assert(pid >= 0);
+        if (pid == 0)
+        {
+
+            if (tieneAmper(vector_cadenas))
+            {
+
+                listaBg = crearProcBg(vector_cadenas);
+
+                procesos[1] = "hola";
+                procesos[2] = "santi";
+
+                pidBG = fork();
+
+                if (pidBG == 0)
+                {
+
+                    execvp(listaBg[0], listaBg);
+                }
+                else if (pidBG > 0)
+                {
+                    exit(0);
+                }
+            }
+            else
+            {
+
+                execvp(vector_cadenas[0], vector_cadenas);
+            }
+
+            for (int i = 0; i <= strlen(*vector_cadenas); i++)
+            {
+
+                if (strcmp(vector_cadenas[i], "detener") == 0)
+                {
+
+                    if (vector_cadenas[1] != NULL)
+                    {
+                        char *const kill[] = {"kill", vector_cadenas[1], NULL};
+                        execvp(kill[0], kill);
+                        printf("El proceso ha sido matado brutalmente...\n");
+                    }
+                    else
+                    {
+                        printf("Rectifica el pid del proceso a detener, con el comando 'ps' puedes saber los pids de todos los procesos actuales \n");
+                    }
+                }
+            }
+
+            execvp(vector_cadenas[0], vector_cadenas);
+        }
+        else
+        {
+
+            wait(NULL);
+        }
+    }
+
+    return 0;
+}
